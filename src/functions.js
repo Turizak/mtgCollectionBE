ce94db795d90d5879d4fdfe5a424258fefd3c302
Rob Kazirut <robert.kazirut@gmail.com>
2022-10-28 20:18:16 -0400

    refactor update all prices to us db query

--- src/functions.js
+++ src/functions.js
@@ -1,43 +1,6 @@
-const axios = require("axios");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const moment = require("moment");
-
-const updateAllPrices = async (token) => {
-  const headers = {
-    "Content-type": "application/json",
-    Authorization: `${token}`,
-  };
-  const cards = await axios
-    .get(`http://localhost:3000/api/v1/account/cards`, {
-      headers: headers,
-    })
-    .then((response) => response.data);
-
-  for (let card of cards) {
-    const scryData = await axios
-      .get(`https://api.scryfall.com/cards/${card.scry_id}`)
-      .then((response) => response.data);
-
-    await axios.patch(
-      `http://localhost:3000/api/v1/account/cards/${card.scry_id}`,
-      {
-        price: scryData.prices.usd,
-      },
-      {
-        headers: headers,
-      }
-    );
-  }
-
-  const newData = await axios
-    .get(`http://localhost:3000/api/v1/account/cards`, {
-      headers: headers,
-    })
-    .then((response) => response.data);
-
-  return newData;
-};
 
 const generatePassword = async (password) => {
   const salt = await bcrypt.genSalt(15);
@@ -66,7 +29,6 @@ const verifyJWT = async (token) => {
 };
 
 module.exports = {
-  updateAllPrices,
   generatePassword,
   generateJWT,
   verifyJWT,
