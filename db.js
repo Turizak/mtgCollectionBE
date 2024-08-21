32d6806113fd76370c367e948bd8aa617c4b47b7
Robert Kazirut <robert.kazirut@gmail.com>
2024-08-21 19:08:57 -0400

    rtard

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
   database: "mtgbe",
-  host: process.env.DB_HOST,
+  host: process.env.DBHOST,
   port: 5432,
   ssl: require,
 });
